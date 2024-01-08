374a3a3f87aca69ed7a7658a82776d88aa869f69
lylest <wackyizzy77@gmail.com>
2024-01-08 16:02:13 +0300

    prediction

--- frontend/src/components/topbar/Topnav.jsx
+++ frontend/src/components/topbar/Topnav.jsx
@@ -3,13 +3,15 @@ import "./topbar.css"
 import logo from "../../assets/logo.png"
 import { Button } from 'primereact/button'
 import { Sidebar } from 'primereact/sidebar'
+import { useNavigate } from 'react-router-dom'
 import Sider from '../sider/Sider'
   
 function Topnav() {
+const navigate = useNavigate()
 const[ visible, setVisible] = useState(false)
   return (
     <div className='top-bar-wrapper'>
-      <div className='logo-box'>
+      <div className='logo-box' onClick={() => navigate("/")}>
         <img src={logo} alt="clcik-gasface logo" loading='lazy' />
       </div>
       <p style={{ color: '#fff'}}>Click-Gas</p>
